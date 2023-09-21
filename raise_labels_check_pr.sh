10c138349f9df6f2bbdce3a49f1b3316503ee422
Ian Goddard <ian.goddard@hudl.com>
2023-09-21 17:15:13 +0100

    editing required labels

--- raise_labels_check_pr.sh
+++ raise_labels_check_pr.sh
@@ -1,5 +1,5 @@
-git branch NOTICK-Update-Required-Labels-Check
-git checkout NOTICK-Update-Required-Labels-Check
+git branch NOTICK-Update-Required-Labels-Check-Elite-2023
+git checkout NOTICK-Update-Required-Labels-Check-Elite-2023
 cp -f ./required-labels.yaml ./tmp_dir/atlas/.github/workflows/required-labels.yaml
 git add -A
 git commit -m "editing required labels"
