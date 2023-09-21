08bc6e5689b29109dc8b47318fb8a1458578d3b2
Ian Goddard <ian.goddard@hudl.com>
2023-09-21 17:12:07 +0100

    more

--- raise_labels_check_pr.sh
+++ raise_labels_check_pr.sh
@@ -1,2 +1,7 @@
 git branch NOTICK-Update-Required-Labels-Check
 git checkout NOTICK-Update-Required-Labels-Check
+cp -f ./required-labels.yaml ./tmp_dir/atlas/.github/workflows/required-labels.yaml
+git add -A
+git commit -m "editing required labels"
+git push
+gh pr create --base main --title "NOTICK-Update-Required-Labels-Check" --body "Editing the yaml file to include new labels used by Elite teams"
