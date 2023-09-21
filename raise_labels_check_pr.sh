570e6e160898f0973fd5eae8297ff65a9c7314aa
Ian Goddard <ian.goddard@hudl.com>
2023-09-21 17:14:23 +0100

    editing required labels

--- raise_labels_check_pr.sh
+++ raise_labels_check_pr.sh
@@ -4,4 +4,4 @@ cp -f ./required-labels.yaml ./tmp_dir/atlas/.github/workflows/required-labels.y
 git add -A
 git commit -m "editing required labels"
 git push
-gh pr create --base main --title "NOTICK-Update-Required-Labels-Check" --body "Editing the yaml file to include new labels used by Elite teams"
+gh pr create --base main --title "NOTICK-Update-Required-Labels-Check" --body "Editing the required-labels.yaml workflow file to include new labels used by Elite teams"
