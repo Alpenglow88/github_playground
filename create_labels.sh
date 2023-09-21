676f6c2403f296d31ce6889777536a83042536eb
Ian Goddard <ian.goddard@hudl.com>
2023-09-21 17:00:41 +0100

    stff

---
+++ create_labels.sh
@@ -0,0 +1,22 @@
+declare -a Repos=$(<repos.txt)
+declare -a LabelData=$(<labels.txt)
+Labels=()
+while IFS= read -r line; do Labels+=("$line"); done <<<"${LabelData[@]}"
+declare -a Labels
+
+mkdir -p tmp_dir
+cd tmp_dir
+path=$(pwd)
+
+# CREATE LABELS
+for repo in ${Repos[@]}; do
+    git clone git@github.com:hudl/$repo.git
+    echo $repo
+    cd $repo
+    for label in "${Labels[@]}"; do
+        # echo $label
+        gh label create "$label"
+    done
+    cd ..
+    rm -rf $repo
+done
---
+++ raise_labels_check_pr.sh
@@ -0,0 +1,2 @@
+git branch NOTICK-Update-Required-Labels-Check
+git checkout NOTICK-Update-Required-Labels-Check
